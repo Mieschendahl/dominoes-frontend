17e96840d34a5032febf86614591d417e08962fd
Mieschendahl <mieschendahl.leonardo@gmail.com>
2026-06-06 23:44:54 +0200

    upated background

--- lib/socket.ts
+++ lib/socket.ts
@@ -7,7 +7,7 @@ import {
 export type AppSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
 
 export const socket: AppSocket = io("http://localhost:4000", {
-  path: "/dominoes",
+  path: "/dominoes/api",
   autoConnect: false,
   reconnection: true,
   reconnectionAttempts: Infinity,
