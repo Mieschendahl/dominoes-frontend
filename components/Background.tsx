860200a9b62b5361126fb663589087640ed89e8b
Mieschendahl <mieschendahl.leonardo@gmail.com>
2026-06-07 09:37:37 +0200

    updated code for subdomain

--- components/Background.tsx
+++ components/Background.tsx
@@ -2,9 +2,7 @@ export function Background() {
   return (
     <div className="pointer-events-none fixed inset-0 overflow-hidden -z-10">
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_0%_0%,rgba(40%,40%,40%,0.9),transparent_200%)]" />
-      <div className="absolute inset-0 bg-[url('/dominoes/noise.svg')] bg-repeat bg-[length:150px_150px] opacity-10" />
+      <div className="absolute inset-0 bg-[url('/noise.svg')] bg-repeat bg-[length:150px_150px] opacity-10" />
     </div>
   );
 }
-
-// TODO: update dominoes prefix
